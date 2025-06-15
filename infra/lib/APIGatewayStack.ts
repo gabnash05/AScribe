1a9d2266d13dcd86595de31cf48c5c43199449d6
KimGab <nasayaokim@gmail.com>
2025-06-15 23:17:02 +0800

    feat(infra): Add StorageStack, Update APIGatewayStack to use RestAPI

--- infra/lib/APIGatewayStack.ts
+++ infra/lib/APIGatewayStack.ts
@@ -1,12 +1,8 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
-import { HttpApi, HttpApiProps, HttpMethod, CorsHttpMethod } from 'aws-cdk-lib/aws-apigatewayv2';
-import { HttpUserPoolAuthorizer } from 'aws-cdk-lib/aws-apigatewayv2-authorizers';
-import { HttpLambdaIntegration } from 'aws-cdk-lib/aws-apigatewayv2-integrations';
-import { UserPool, UserPoolClient } from 'aws-cdk-lib/aws-cognito';
-import { ApiGateway } from 'aws-cdk-lib/aws-events-targets';
 import { Construct } from 'constructs';
+import { RestApi, LambdaIntegration, CognitoUserPoolsAuthorizer, Cors, AuthorizationType } from 'aws-cdk-lib/aws-apigateway';
+import { UserPool, UserPoolClient } from 'aws-cdk-lib/aws-cognito';
 import { IFunction } from 'aws-cdk-lib/aws-lambda';
-
 
 interface APIGatewayStackProps extends StackProps {
     userPool: UserPool;
@@ -20,64 +16,60 @@ interface APIGatewayStackProps extends StackProps {
     summarizeLambda: IFunction;
     generateFlashCardsLambda: IFunction;
     generateQuizLambda: IFunction;
-    // Add other Lambda functions as needed
 }
 
 export class APIGatewayStack extends Stack {
-    public readonly httpApi: HttpApi;
+    public readonly restApi: RestApi;
 
     constructor(scope: Construct, id: string, props: APIGatewayStackProps) {
         super(scope, id, props);
 
-        const { 
-            userPool, 
-            userPoolClient, 
-            uploadLambda, 
-            finalizeUploadLambda, 
-            searchLambda, 
-            getDocumentLambda, 
-            deleteDocumentLambda, 
-            updateDocumentLambda, 
+        const {
+            userPool,
+            uploadLambda,
+            finalizeUploadLambda,
+            searchLambda,
+            getDocumentLambda,
+            deleteDocumentLambda,
+            updateDocumentLambda,
             summarizeLambda,
             generateFlashCardsLambda,
-            generateQuizLambda,
+            generateQuizLambda
         } = props;
 
-        const authorizer = this.createUserPoolAuthorizer(userPool, userPoolClient);
-
-        this.httpApi = new HttpApi(this, 'StudentDocHttpApi', {
-            description: 'HTTP API for student OCR app',
-            corsPreflight: {
+        this.restApi = new RestApi(this, 'AScribeRestApi', {
+            description: 'AScribe REST API',
+            defaultCorsPreflightOptions: {
                 allowHeaders: ['Authorization', 'Content-Type'],
-                allowMethods: [CorsHttpMethod.POST, CorsHttpMethod.GET],
-                allowOrigins: ['*'], // change to your domain in prod
+                allowMethods: Cors.ALL_METHODS,
+                allowOrigins: Cors.ALL_ORIGINS, // restrict in production
             },
         });
 
-        this.addRoute('/upload', HttpMethod.POST, uploadLambda, authorizer);
-        this.addRoute('/finalizeUpload', HttpMethod.POST, finalizeUploadLambda, authorizer);
-        this.addRoute('/summarize', HttpMethod.GET, summarizeLambda, authorizer);
-        this.addRoute('/search', HttpMethod.GET, searchLambda, authorizer);
-        this.addRoute('/get', HttpMethod.GET, getDocumentLambda, authorizer);
-        this.addRoute('/delete', HttpMethod.DELETE, deleteDocumentLambda, authorizer);
-        this.addRoute('/update', HttpMethod.PUT, updateDocumentLambda, authorizer);
-        this.addRoute('/generateFlashCards', HttpMethod.POST, generateFlashCardsLambda, authorizer);
-        this.addRoute('/generateQuiz', HttpMethod.POST, generateQuizLambda, authorizer);
-        // Add other routes as needed
-    }
-
-    private createUserPoolAuthorizer(userPool: UserPool, userPoolClient: UserPoolClient): HttpUserPoolAuthorizer {
-        return new HttpUserPoolAuthorizer('CognitoAuthorizer', userPool, {
-            userPoolClients: [userPoolClient],
+        const authorizer = new CognitoUserPoolsAuthorizer(this, 'CognitoAuthorizer', {
+            cognitoUserPools: [userPool],
         });
+
+        this.addLambdaRoute('upload', 'POST', uploadLambda, authorizer);
+        this.addLambdaRoute('finalizeUpload', 'POST', finalizeUploadLambda, authorizer);
+        this.addLambdaRoute('search', 'GET', searchLambda, authorizer);
+        this.addLambdaRoute('get', 'GET', getDocumentLambda, authorizer);
+        this.addLambdaRoute('delete', 'DELETE', deleteDocumentLambda, authorizer);
+        this.addLambdaRoute('update', 'PUT', updateDocumentLambda, authorizer);
+        this.addLambdaRoute('summarize', 'GET', summarizeLambda, authorizer);
+        this.addLambdaRoute('generateFlashCards', 'POST', generateFlashCardsLambda, authorizer);
+        this.addLambdaRoute('generateQuiz', 'POST', generateQuizLambda, authorizer);
     }
 
-    private addRoute(path: string, method: HttpMethod, handler: IFunction, authorizer: HttpUserPoolAuthorizer): void {
-        const integration = new HttpLambdaIntegration(`${path}Integration`, handler);
-        this.httpApi.addRoutes({
-            path,
-            methods: [method],
-            integration,
+    private addLambdaRoute(
+        path: string,
+        method: string,
+        lambdaFn: IFunction,
+        authorizer: CognitoUserPoolsAuthorizer
+    ): void {
+        const resource = this.restApi.root.addResource(path);
+        resource.addMethod(method, new LambdaIntegration(lambdaFn), {
+            authorizationType: AuthorizationType.COGNITO,
             authorizer,
         });
     }
--- infra/lib/AuthStack.ts
+++ infra/lib/AuthStack.ts
@@ -57,4 +57,4 @@ export class AuthStack extends Stack {
             },
         });
     }
-}   
+}
