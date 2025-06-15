22822b42ee8ee8b5abbffa04d3650a8b7f8d66e4
KimGab <nasayaokim@gmail.com>
2025-06-15 16:45:41 +0800

    feat(infra): Add APIGatewayStack and AuthStack, Fix infra and backend directory

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -1,20 +1,28 @@
 #!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { InfraStack } from '../lib/infra-stack';
+import {App, StackProps} from 'aws-cdk-lib';
+import { AuthStack } from '../lib/AuthStack';
+import { APIGatewayStack } from '../lib/APIGatewayStack';
 
-const app = new cdk.App();
-new InfraStack(app, 'InfraStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
+const app = new App();
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+const env: StackProps = {
+    env: {
+        account: process.env.CDK_DEFAULT_ACCOUNT,
+        region: process.env.CDK_DEFAULT_REGION,
+    },
+}
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+const authStack = new AuthStack(app, 'AuthStack', env);
 
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+// new APIGatewayStack(app, 'StudentDocAPIGatewayStack', {
+//     ...env,
+//     userPool: authStack.userPool,
+//     userPoolClient: authStack.userPoolClient,
+//     uploadLambda: lambdaStack.uploadLambda,
+//     finalizeLambda: lambdaStack.finalizeLambda,
+//     summarizeLambda: lambdaStack.summarizeLambda,
+//     searchLambda: lambdaStack.searchLambda,
+//     getLambda: lambdaStack.getLambda,
+//     deleteLambda: lambdaStack.deleteLambda,
+//     updateLambda: lambdaStack.updateLambda,
+// });
---
+++ infra/lib/AuthStack.ts
@@ -0,0 +1,60 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { UserPool, UserPoolClient, AccountRecovery, VerificationEmailStyle, UserPoolClientIdentityProvider } from 'aws-cdk-lib/aws-cognito';
+
+import { Construct } from 'constructs';
+
+interface AuthStackProps extends StackProps {}
+
+export class AuthStack extends Stack {
+    public readonly userPool: UserPool;
+    public readonly userPoolClient: UserPoolClient;
+	
+    constructor(scope: Construct, id: string, props?: AuthStackProps) {
+        super(scope, id, props);
+
+        this.userPool = new UserPool(this, 'AScribeUserPool', {
+            userPoolName: 'AScribeUserPool',
+            selfSignUpEnabled: true,
+            signInAliases: {
+                email: true,
+                username: true,
+                phone: false
+            },
+            autoVerify: {
+                email: true,
+            },
+            accountRecovery: AccountRecovery.EMAIL_ONLY,
+            standardAttributes: {
+                email: {
+                    required: true,
+                    mutable: false,
+                },
+                givenName: {
+                    required: true,
+                    mutable: true,
+                }
+            },
+            userVerification: {
+                emailSubject: 'Verify your email for AScribe',
+                emailBody: 'Hello {username},\n\nPlease verify your email by clicking on the following link: {##Verify Email##}',
+                emailStyle: VerificationEmailStyle.LINK,
+            },
+            userInvitation: {
+                emailSubject: 'You have been invited to AScribe',
+                emailBody: 'Hello {username},\n\nYou have been invited to join AScribe. Please use the following temporary password to log in: {####}',
+            }
+        });
+
+        this.userPoolClient = new UserPoolClient(this, 'AScribeUserPoolClient', {
+            userPool: this.userPool,
+            userPoolClientName: 'AScribeUserPoolClient',
+            supportedIdentityProviders: [UserPoolClientIdentityProvider.COGNITO],
+            generateSecret: false,
+            authFlows: {
+                userPassword: true,
+                userSrp: true,
+                adminUserPassword: true,
+            },
+        });
+    }
+}   
