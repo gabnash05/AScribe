e4d898d58f97cd4e89ec54f43c27167abd428077
KimGab <nasayaokim@gmail.com>
2025-06-15 23:17:02 +0800

    feat(infra): Add StorageStack, Update APIGatewayStack to use RestAPI

---
+++ infra/lib/StorageStack.ts
@@ -0,0 +1,28 @@
+import { RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
+import { UserPool } from 'aws-cdk-lib/aws-cognito';
+import { BlockPublicAccess, Bucket, BucketEncryption, HttpMethods } from 'aws-cdk-lib/aws-s3';
+import { Construct } from 'constructs';
+
+export class StorageStack extends Stack {
+    public readonly documentBucket: Bucket
+
+    constructor(scope: Construct, id: string, props?: StackProps) {
+        super(scope, id, props);
+        
+        this.documentBucket = new Bucket(this, 'DocumentBucket', {
+            bucketName: 'ascribe-document-bucket',
+            removalPolicy: RemovalPolicy.RETAIN, // retain data across stack deletions
+            encryption: BucketEncryption.S3_MANAGED,
+            blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+            cors: [
+                {
+                    allowedMethods: [HttpMethods.GET, HttpMethods.POST, HttpMethods.PUT, HttpMethods.DELETE],
+                    allowedOrigins: ['*'], // Change to your domain in production
+                    allowedHeaders: ['*'],
+                    maxAge: 3000,
+                    exposedHeaders: ['ETag']
+                }
+            ]
+        });
+    }
+}
